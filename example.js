a7c3d92262838d1d531a8126ffab1944f6e6efb2
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2021-08-20 22:28:34 +0200

    Create example.js

---
+++ example.js
@@ -0,0 +1,6 @@
+screens2d.canvas.init("kangaroo")
+screens2d.canvas.rect(0, 0, "full", "full", 0, "white")
+screens2d.canvas.rect(0, 0, "full", 50, 0, "brown")
+screens2d.canvas.text(20, 30, "Roboto", "500 18px", "About kangaroos", "white")
+screens2d.canvas.text(20, 80, "Roboto", "16px", "I like kangaroos.", "black")
+screens2d.canvas.image(20, 100, "kangaroo.jpeg", 200, 300)
